31a157ba9d17a9918b59445d7dc9c93d2c68cf97
richardmachage <rchacha261@gmail.com>
2024-06-05 00:29:40 +0300

    fix(hilt): dI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
 }
 
 android {
@@ -66,4 +68,12 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //dagger hilt
+    //hilt
+    implementation(libs.hiltAndroid)
+    //hilt viewmodel
+    implementation(libs.hiltNavigation)
+    //ksp hilt compiler
+    ksp(libs.hiltCompiler)
 }
---
+++ app/src/main/java/com/forsythe/iris/Application.kt
@@ -0,0 +1,7 @@
+package com.forsythe.iris
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class Application : Application() {
+}

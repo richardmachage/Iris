cf439992882a3d1b4a321f199585a7d8fffe8581
richardmachage <rchacha261@gmail.com>
2024-06-05 18:40:15 +0300

    add(di):   provides iris database

--- app/src/main/java/com/forsythe/iris/di/AppModule.kt
+++ app/src/main/java/com/forsythe/iris/di/AppModule.kt
@@ -1,11 +1,20 @@
 package com.forsythe.iris.di
 
+import com.forsythe.iris.Application
+import com.forsythe.iris.data.room.IrisDatabase
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Provides
+    @Singleton
+    fun providesIrisDatabase(application: Application):IrisDatabase = IrisDatabase.getInstance(application)
+
 
 }

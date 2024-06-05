ccdfbb1c70eb7b0e272498c2549b63e6d6daf06b
richardmachage <rchacha261@gmail.com>
2024-06-05 22:23:04 +0300

    fix(core): correctly identifies type of transaction

--- app/src/main/java/com/forsythe/iris/models/TransactionType.kt
+++ app/src/main/java/com/forsythe/iris/models/TransactionType.kt
@@ -1,6 +1,7 @@
 package com.forsythe.iris.models
 class TransactionType{
     companion object{
+        const val none = "not specified"
         const val send  = "send money"
         const val receive  = "receive money"
         const val payBill  = "pay bill"
--- app/src/main/java/com/forsythe/iris/services/SmsReceiver.kt
+++ app/src/main/java/com/forsythe/iris/services/SmsReceiver.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import android.provider.Telephony
 import android.util.Log
 import android.widget.Toast
+import com.forsythe.iris.models.TransactionType
 
 class SmsReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
@@ -15,36 +16,40 @@ class SmsReceiver : BroadcastReceiver() {
             Log.d("receiver", "broadcast fired")
             val bundle = intent.extras
             bundle?.let {
-                val pdus = bundle.get("pdus") as Array<*>
+                val messages = Telephony.Sms.Intents.getMessagesFromIntent(intent)
+                val fullMessage = StringBuilder()
 
-                for (pdu in pdus) {
-                    val msg = Telephony.Sms.Intents.getMessagesFromIntent(intent)
-                    for (sms in msg) {
-                        myMessage.body = sms.messageBody
-                        myMessage.originatingAddress = sms.originatingAddress.toString()
-
-                    }
+                for (sms in messages){
+                    fullMessage.append(sms.messageBody)
+                    myMessage.originatingAddress = sms.originatingAddress.toString()
                 }
-                //Log.d("receiver", "SMS received from ${myMessage.originatingAddress}: ${myMessage.body}")
+                myMessage.body = fullMessage.toString()
+                Log.d("receiver", "SMS received from ${myMessage.originatingAddress}: ${myMessage.body}")
             }
 
             //TODO
             /*
             1.extract required info from the body
+            -check the message type first, then apply appropriate regex
+
+
             2. save the info to local Room
            */
+            var type : String?=null
             if (myMessage.originatingAddress == "MPESA") {
-                extractMpesaDetails(myMessage.body)?.let {
-                    Log.d(
-                        "receiver",
-                        "SMS received from ${myMessage.originatingAddress}: ${myMessage.body}"
-                    )
-                    Log.d("mpesa details", "Received from ${it.person}")
-                } ?: {
-                    Log.d("mpesa details", "mpesa details is null")
+                when{
+                    myMessage.body.contains("sent to") && myMessage.body.contains("for account") -> type = TransactionType.payBill
+                    myMessage.body.contains("sent to", ignoreCase = true) -> type = TransactionType.send
+                    myMessage.body.contains("received", ignoreCase = true) -> type = TransactionType.receive
+                    myMessage.body.contains("paid to", ignoreCase = true) -> type = TransactionType.till
+                    else -> type =  TransactionType.none
                 }
             }
-
+            type?.let {
+                Log.d("Mpesa", "Transaction Type: $it")
+            }?:{
+                Log.d("Mpesa", "Transaction Type: null")
+            }
             /*Log.d("mpesa details", "Amount Received ${mpesaDetails.person}")
             Log.d("mpesa details", "Balance ${mpesaDetails.balance}")
             Log.d("mpesa details", "Type of transaction ${mpesaDetails.messageType}")

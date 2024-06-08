994d71443fa0075d713fcc4ba9c3e8a22e59df0e
richardmachage <rchacha261@gmail.com>
2024-06-08 22:46:16 +0300

    fix(): getReceivingAddress fun

--- app/src/main/java/com/forsythe/iris/utils/getRecevingAddress.kt
+++ app/src/main/java/com/forsythe/iris/utils/getRecevingAddress.kt
@@ -1,10 +1,26 @@
 package com.forsythe.iris.utils
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
 import android.telephony.SubscriptionInfo
 import android.telephony.SubscriptionManager
+import androidx.core.app.ActivityCompat
 
-fun getReceivingAddress(context: Context, subscriptionId : Int) : String{
+fun getReceivingAddress(context: Context, subscriptionId : Int) : String?{
     val subscriptionManager = context.getSystemService(Context.TELEPHONY_SUBSCRIPTION_SERVICE) as SubscriptionManager
-    return subscriptionManager.getPhoneNumber(subscriptionId)
+    return if (ActivityCompat.checkSelfPermission(
+            context,
+            Manifest.permission.READ_PHONE_NUMBERS
+        ) == PackageManager.PERMISSION_GRANTED
+    ) {
+        // TODO: Consider calling
+        //    ActivityCompat#requestPermissions
+        // here to request the missing permissions, and then overriding
+        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+        //                                          int[] grantResults)
+        // to handle the case where the user grants the permission. See the documentation
+        // for ActivityCompat#requestPermissions for more details.
+        return     subscriptionManager.getPhoneNumber(subscriptionId)
+    }else null
 }

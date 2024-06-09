1609cfadb9d9118568e661dd6e35a68068b31e32
richardmachage <rchacha261@gmail.com>
2024-06-09 21:15:12 +0300

    add (Presentation): home

--- app/src/main/java/com/forsythe/iris/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/forsythe/iris/ui/screens/home/HomeScreen.kt
@@ -1,2 +1,31 @@
 package com.forsythe.iris.ui.screens.home
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HomeScreen(){
+    Scaffold(
+    ) {padding->
+        Surface(
+            color = MaterialTheme.colorScheme.background,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+        ) {
+            LazyColumn {
+                items(count = 10){
+                    Text(text = "Meesage item $it")
+                }
+            }
+        }
+    }
+}
